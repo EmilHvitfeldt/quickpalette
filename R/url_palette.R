182d2a5873009106247e1924e595cf0c99a0a25f
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2019-03-30 09:08:37 -0700

    remove mscarts palettes

--- R/url_palette.R
+++ R/url_palette.R
@@ -30,7 +30,8 @@
 #'     "hunterlab", "lch", "luv", "rgb", "xyz", "yxy".
 #' @return A color palette as a string.
 #' @examples
-#' url <- "https://raw.githubusercontent.com/EmilHvitfeldt/quickpalette/master/man/figures/README-testchart-1.png"
+#' url <- paste0("https://raw.githubusercontent.com/EmilHvitfeldt/quickpalette/",
+#'               "master/man/figures/README-testchart-1.png")
 #' url_palette(url, n_clusters = 5)
 #' @export
 url_palette <- function(url, n_clusters = 1, rm_color = c("greyscale"),
